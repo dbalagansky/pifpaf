21f9e2ab636be2b0269a74fdf1c1f9e3478f34dc
Danila Balagansky <dbalagansky@me.com>
2025-05-08 21:41:32 +0300

    Fix influxdb wait_for_line

--- pifpaf/drivers/influxdb.py
+++ pifpaf/drivers/influxdb.py
@@ -69,7 +69,7 @@ class InfluxDBDriver(drivers.Driver):
         c, _ = self._exec(
             ["influxd", "-config", cfgfile],
             wait_for_line=(
-                r"Listening on HTTP: \[::\]:%d" % self.port
+                r"Listening on HTTP.*\[::\]:%d" % self.port
             ),
             path=["/opt/influxdb"])
 
